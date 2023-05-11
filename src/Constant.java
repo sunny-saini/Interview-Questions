29be6781804f14be51ff829051977019c91245dc
sunny-saini <48470642+sunny-saini@users.noreply.github.com>
2023-05-11 15:51:46 +0530

    initial code commit

---
+++ src/Constant.java
@@ -0,0 +1,5 @@
+public enum Constant {
+    CLASS,
+    ROOM,
+    FLAWERS
+}
---
+++ src/UseEnum.java
@@ -0,0 +1,10 @@
+public class UseEnum {
+    public static void main(String[] args) {
+        System.out.println(Constant.ROOM);
+        final Constant flawers = Constant.FLAWERS;
+        System.out.println(flawers);//
+
+
+
+    }
+}

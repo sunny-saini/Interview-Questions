be2727729e7646ee6501eca68c22aefb08713f9c
sunny-saini <48470642+sunny-saini@users.noreply.github.com>
2023-05-12 09:39:56 +0530

    my first commit

---
+++ src/SecondMax.java
@@ -0,0 +1,32 @@
+public class SecondMax {
+
+    public static void main(String[] args) {
+        int arr[]={1,2,4,5,6,75,34,34,75,75,75,75,12,9,10,10,1000001};
+
+        int max2=0;
+        int max1=0;
+
+        /*for(int i=0;i<arr.length;i++){
+            if(max1<arr[i]){
+                max2=max1;
+                max1=arr[i];
+            }
+            else if(max2<arr[i] && max1!=arr[i]){
+                max2=arr[i];
+            }
+        }*/
+
+        for(int num:arr){
+            if(max1<num){
+                max2=max1;
+                max1=num;
+            } else if (max2<num && max1!=num) {
+                max2=num;
+            }
+        }
+
+
+        System.out.println("max1 "+max1);
+        System.out.println("max2 "+max2);
+    }
+}

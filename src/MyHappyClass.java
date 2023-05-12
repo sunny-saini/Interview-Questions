8d61909c7c509898cd50c8841fcff29c0d2a7ed7
sunny-saini <48470642+sunny-saini@users.noreply.github.com>
2023-05-12 12:27:55 +0530

    roygh

--- src/MyHappyClass.java
+++ src/MyHappyClass.java
@@ -1,3 +1,19 @@
 public class MyHappyClass {
     //Write Code Here
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("hello");
+
+        System.out.println("how");
+
+        System.out.println("are");
+
+        System.out.println("u");
+
+    }
 }
